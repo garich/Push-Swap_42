585e855a809664d360ca20209973b058640111d1
Alvaro Garijo Duerto <alvarogd@MacBook-Pro-de-Alvaro.local>
2023-03-23 02:18:27 +0100

    Still missing the only-one-argument as string function for arguments. For the moment no memory leaks. Separated main() into two functions: fill_list_with_array(); works properly.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: agarijo- <agarijo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 12:02:11 by agarijo-          #+#    #+#             */
-/*   Updated: 2023/03/22 17:10:18 by agarijo-         ###   ########.fr       */
+/*   Updated: 2023/03/22 22:54:40 by agarijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,31 @@ void	leaks(void)
 	system("leaks -q --list push_swap");
 }
 
+t_node	*fill_list_with_array(int *array, int argc)
+{
+	int		counter;
+	t_node	*node;
+	t_node	*head;
+
+	node = NULL;
+	head = NULL;
+	counter = 0;
+	while ((counter + 1) < argc)
+	{
+		node = lst_new_node();
+		lst_add_value_to_node(node, array[counter]);
+		lst_add_back(&head, node);
+		counter++;
+	}
+	return (head);
+}
+
 int	main(int argc, char *argv[])
 {
-	int		status;
-	int		counter;
 	int		*array_arguments;
 	t_node	*head;
-	t_node	*node;
 
 	array_arguments = NULL;
-	status = -1;
-	counter = 0;
 	head = NULL;
 	if (argc == 1)
 		exit(1);
@@ -36,13 +50,7 @@ int	main(int argc, char *argv[])
 		printf("Error\n");
 	else
 	{
-		while ((counter + 1) < argc)
-		{
-			node = lst_new_node();
-			lst_add_value_to_node(node, array_arguments[counter]);
-			lst_add_back(&head, node);
-			counter++;
-		}
+		head = fill_list_with_array(array_arguments, argc);
 		lst_print(head);
 		lst_clear(&head);
 		free(array_arguments);
